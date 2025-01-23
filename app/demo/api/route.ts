64e25dcc15fc01d1090bb78b7eca9a09f051157c
melvynxdev <melvynmal@gmail.com>
2025-01-23 16:32:19 +0800

    fix it

--- app/demo/api/route.ts
+++ app/demo/api/route.ts
@@ -2,5 +2,5 @@ export const dynamic = "force-static";
 
 export const GET = async () => {
   await new Promise((r) => setTimeout(r, 1000));
-  return new Response();
+  return new Response(Math.random().toString());
 };
