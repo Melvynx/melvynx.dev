4790133093fb93abf63464542cef8c7a80fceeb2
melvynxdev <melvynmal@gmail.com>
2025-01-23 13:19:59 +0800

    fix it

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { action } from "./action";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const fetchServerAction = async () => {
   return action();
 };
@@ -15,7 +15,8 @@ export default function Page() {
   const [data, setData] = useState<string[]>([]);
   return (
     <div className="m-auto flex max-w-xl flex-col gap-4 p-8">
-      <Button
+      <button
+        className="bg-black text-white rounded-md px-4 py-2"
         onClick={async () => {
           setData([]);
           const data = await Promise.all([
@@ -27,7 +28,7 @@ export default function Page() {
         }}
       >
         Click me
-      </Button>
+      </button>
       {data.map((d) => (
         <p key={d}>{d}</p>
       ))}
