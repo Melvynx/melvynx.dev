f1aa4741bd6fe013f61a8a8ba5983c570732053d
melvynxdev <melvynmal@gmail.com>
2025-01-23 13:17:05 +0800

    add melvynx.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,149 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="max-w-2xl mx-auto py-4 px-4 lg:pt-8 min-h-full flex flex-col gap-12">
+      <header className="flex flex-col gap-4">
+        <h1 className="font-caption text-2xl lg:text-3xl font-bold">
+          Melvyn Malherbe
+        </h1>
+        <p className="text-neutral-700">
+          I'm a software engineer and entrepreneur. I'm the founder of{" "}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="https://codelynx.dev"
+            className="text-blue-500 hover:underline"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Codelynx
           </a>
+          , online coding courses and tutorials. I create content on{" "}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="https://mlv.sh/youtube"
+            className="text-blue-500 hover:underline"
           >
-            Read our docs
-          </a>
+            YouTube
+          </a>{" "}
+          to help developer to increase their competences.
+        </p>
+      </header>
+      <main className="flex-1 flex flex-col gap-6 lg:gap-8">
+        <div className="flex flex-col gap-1">
+          <h2 className="font-caption text-lg lg:text-xl font-medium">
+            Experiences
+          </h2>
+          <ul className="list-disc list-inside">
+            <li>
+              <span className="font-medium">Full-Stack Online Trainer</span> at{" "}
+              <a
+                className="text-blue-500 hover:underline"
+                href="https://codelynx.dev"
+              >
+                Codelynx.dev
+              </a>{" "}
+              <span className="text-neutral-700">- 2022 - now</span>
+            </li>
+            <li>
+              <span className="font-medium">Full-stack Freelance</span> at YuZu{" "}
+              <span className="text-neutral-700">- 2022</span>
+            </li>
+            <li>
+              <span className="font-medium">Software Engineer</span> at{" "}
+              <a
+                className="text-blue-500 hover:underline"
+                href="https://qoqa.ch"
+              >
+                QoQa.ch
+              </a>{" "}
+              <span className="text-neutral-700">- 2018 - 2022</span>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h2 className="font-caption text-lg lg:text-xl font-medium">
+            Projects
+          </h2>
+          <ul className="list-disc list-inside">
+            <li>
+              <a
+                href="https://codeline.app"
+                className="text-blue-500 font-medium hover:underline"
+              >
+                Codeline.app
+              </a>{" "}
+              - Online developer courses LMS
+            </li>
+            <li>
+              <a
+                href="https://chat2code.dev"
+                className="text-blue-500 font-medium hover:underline"
+              >
+                Chat2Code
+              </a>{" "}
+              - AI-powered frontend code generation tool
+            </li>
+            <li>
+              <a
+                href="https://quizup.app"
+                className="text-blue-500 font-medium hover:underline"
+              >
+                QuizUp
+              </a>{" "}
+              - AI-powered quiz generation platform
+            </li>
+            <li>
+              <a
+                href="https://bulkcorrector"
+                className="text-blue-500 font-medium hover:underline"
+              >
+                BulkCorrector
+              </a>{" "}
+              - AI grammar correction tool for large texts
+            </li>
+            <li>
+              <a
+                href="https://askschema.com"
+                className="text-blue-500 font-medium hover:underline"
+              >
+                AskSchema
+              </a>{" "}
+              - Chat with your database easily
+            </li>
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="flex items-center gap-4 lg:gap-8 flex-wrap text-neutral-700">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="text-neutral-700 hover:underline"
+          href="https://mlv.sh/twitter"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          X
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="text-neutral-700 hover:underline"
+          href="https://mlv.sh/linkedin"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          LinkedIn
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="text-neutral-700 hover:underline"
+          href="https://mlv.sh/github"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          GitHub
+        </a>
+        <a
+          className="text-neutral-700 hover:underline"
+          href="https://mlv.sh/youtube"
+        >
+          YouTube
+        </a>
+        <a
+          className="text-neutral-700 hover:underline"
+          href="https://codelynx.dev/posts"
+        >
+          Blog
+        </a>
+        <a
+          className="text-neutral-700 hover:underline"
+          href="https://melvynx.com"
+        >
+          Melvynx.com
         </a>
       </footer>
     </div>
